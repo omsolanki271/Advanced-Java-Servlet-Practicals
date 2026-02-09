470c0505fe7ac3ee0b34920920da7dc989b9f95a
omsolanki271 <omsolanki271.gmail.com>
2026-02-09 22:41:31 +0530

    Generic practical

---
+++ 01_GenericServlet_Project/src/main/java/com/aoot/generic/Program2.java
@@ -0,0 +1,30 @@
+package com.aoot.generic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+
+//@WebServlet("/Program2")
+public class Program2 extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+
+    	public void init(ServletConfig config) throws ServletException {
+    		 System.out.println("Program2 servlet initialized");
+	}
+
+	
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>GenericServlet Program 2</h1>");
+        out.println("<p>init() method executed</p>");
+	}
+
+}
