480fff1b2e4a7488eb6fc6bd500c82827c9dc38e
omsolanki271 <omsolanki271@gmail.com>
2026-03-19 02:28:51 +0530

    Login work perfect

--- HireHub/src/main/java/com/hirehub/user/LoginServlet.java
+++ HireHub/src/main/java/com/hirehub/user/LoginServlet.java
@@ -3,7 +3,7 @@ package com.hirehub.user;
 import java.awt.geom.Rectangle2D;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
+import java.sql.*;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,9 +11,11 @@ import java.sql.ResultSet;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet("/LoginServlet")
@@ -28,35 +30,62 @@ public class LoginServlet extends HttpServlet {
 		PrintWriter out = res.getWriter();
 		
 		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/jobportal", "root", "abc123");
-			
+
 			String sql = "select * from users where email =? and pass=?";
 			
-			PreparedStatement ps = conn.prepareStatement(sql);
+			ps = conn.prepareStatement(sql);
 			
 			ps.setString(1, email);
 			ps.setString(2, password);
 			
-			ResultSet rs = ps.executeQuery();
+			rs = ps.executeQuery();
 			
 			if(rs.next())
 			{
+
+				HttpSession session = req.getSession();
+				session.setAttribute("userEmail", email);
+				Cookie cookie = new Cookie("userEmail", email);
+				cookie.setMaxAge(60 * 60);
+				res.addCookie(cookie);
+				
 				res.sendRedirect("Adashboard.html");
 			}
 			else
 			{
-				out.println("Invalid username or password");
-				RequestDispatcher rd = req.getRequestDispatcher("login.html");
-				rd.include(req, res);
+				out.println("<h3>Invalid Email or Password</h3>");
 			}
 			
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
+		}
+		try {
+			
+		} finally {
+			try {
+				if(conn != null)
+				{
+					conn.close();
+				}
+				if(ps != null)
+				{
+					ps.close();
+				}
+				if(rs != null)
+				{
+					rs.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -66,3 +95,4 @@ public class LoginServlet extends HttpServlet {
 	}
 
 }
+
